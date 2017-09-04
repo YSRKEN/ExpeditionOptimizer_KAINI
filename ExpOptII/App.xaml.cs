969c3ac8b438271620ed9d2b87ceef308ad2cc80
YSRKEN <ysr.ken@gmail.com>
2017-09-05 03:00:03 +0900

    とりあえずコンパイルして実行まではできるようにした

--- ExpOptII/App.xaml.cs
+++ ExpOptII/App.xaml.cs
@@ -11,5 +11,11 @@ namespace ExpOptII {
 	/// App.xaml の相互作用ロジック
 	/// </summary>
 	public partial class App : Application {
+		protected override void OnStartup(StartupEventArgs e) {
+			base.OnStartup(e);
+
+			var bootstrapper = new Bootstrapper();
+			bootstrapper.Run();
+		}
 	}
 }
